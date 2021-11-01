5734b1ec2a5c5089ac52d63787308e5117d9b949
HYEONGDALKIM <guddak@kakao.com>
2021-11-01 13:28:47 +0900

    finish

--- Nodejs-master/nodejs/cookie.js
+++ Nodejs-master/nodejs/cookie.js
@@ -12,10 +12,12 @@ http.createServer(function(request, response){
     response.writeHead(200, {
         'Set-Cookie':['yummy_cookie=choco', 
         'tasty_cookie=strawberry',
-        `Permanent=cookies; Max-Age=${60*60*24*30}`,
+        `Permanent=cookies; Max-Age=${60*60*24*30}`, // 24시간 지정
         'Secure=Secure; Secure',  // 뒤에 있는게 value를 가진다.
-        'HttpOnly=HttpOnly; HttpOnly'
-    ]
+        'HttpOnly=HttpOnly; HttpOnly',
+        'Path=Path; Path=/cookie', //특정  디렉터리에 대한 쿠키를 가지고 싶을 때
+        'Domain=Domain; Domain=o2.org'
+        ]
     });
     response.end('Cookie!!');
 }).listen(3000);
